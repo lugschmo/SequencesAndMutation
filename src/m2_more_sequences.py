bf025433435febde8bbf129379d634f908006272
lugschmo <lugschmo@rose-hulman.edu>
2019-07-19 18:46:48 -0600

    done 3

--- src/m2_more_sequences.py
+++ src/m2_more_sequences.py
@@ -10,8 +10,8 @@ for ITERATING through SEQUENCES, including selections from:
   -- Looking at two sequences in parallel
 
 Authors: David Mutchler, Valerie Galluzzi, Mark Hays, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Mitch Lugsch.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 
 def main():
@@ -103,9 +103,14 @@ def shortest_string(strings):
       :type strings: list[str]   or tuple(str)
     """
     # ------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # DONE: 2. Implement and test this function.
     #     The testing code is already written for you (above).
     # ------------------------------------------------------------------
+    k_for_min = 0
+    for k in range(1, len(strings)):
+        if len(strings[k]) < len(strings[k_for_min]):
+            k_for_min = k
+    return strings[k_for_min]
 
 
 def run_test_index_of_largest_number():
@@ -180,9 +185,14 @@ def index_of_largest_number(numbers, n):
 
     """
     # ------------------------------------------------------------------
-    # TODO: 3. Implement and test this function.
+    # DONE: 3. Implement and test this function.
     #     The testing code is already written for you (above).
     # ------------------------------------------------------------------
+    k_for_max = 0
+    for k in range(1, n):
+        if numbers[k] > numbers[k_for_max]:
+            k_for_max = k
+    return k_for_max
 
 
 # ----------------------------------------------------------------------
